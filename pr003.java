4adf16a9bad7e559d5217a13e1926f8bb9721330
chtovamnad <vita_vita_vita@internet.ru>
2024-09-17 13:34:27 +0700

    Добавили программу для чтения с клавиатуры

---
+++ pr003.java
@@ -0,0 +1,12 @@
+class pr003 {
+	public static void main (String args[]) throws java.io.IOException {
+	
+		//Чтение с клавиатуры
+		char ch;
+		System.out.print("Нажмите клавишу на клавиатуре и ENTER ");
+		ch = (char) System.in.read();
+		System.out.println("Вы нажали клавишу: " + ch);
+		
+	
+	}
+}
