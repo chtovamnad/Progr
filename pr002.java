cd6586923f941515449c5598d9ff0eb16d8f0004
chtovamnad <vita_vita_vita@internet.ru>
2024-09-17 13:18:42 +0700

    Добавлен пример неожиданного повышения типа byte до int

--- pr002.java
+++ pr002.java
@@ -82,6 +82,17 @@ class pr002 {
 		p = false; q = false;
                 System.out.println(p + "\t" + q + "\t");
                 System.out.println((p&q) + "\t" + (p|q) +"\t");
-                System.out.println((p^q) + "\t" + (!p));	
+                System.out.println((p^q) + "\t" + (!p));
+
+		//Пример неожиданного	повышения типов данных
+		System.out.println("\n");
+		byte b11;
+		int i11;
+		b11 = 10;
+		i11 = b11 * b11; //При операции перемножения двух переменных типов byte получается повышение результата до типа int
+
+		b11 = 10;
+		b11 = (byte)(b11 * b11); //Необходимо приведение типа int к типу byte
+		System.out.println("i11 и b11: " + i11 + " " + b11);
 	}
 }
