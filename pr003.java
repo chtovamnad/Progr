5dd90f5ef043c4fbc2bcdb041d3ffb35cbbdf50a
chtovamnad <vita_vita_vita@internet.ru>
2024-09-24 13:42:31 +0700

    Добавили примеры программ с циклом while

--- pr003.java
+++ pr003.java
@@ -140,5 +140,29 @@ class pr003 {
 		//Вычисление сумм чисел от 1 до 5
 		for(i8=1;i8<=5;sum+=i8++);
 		System.out.println("Сумма чисел равна " + sum);
+
+		System.out.println("\n");
+		//Цикл while
+		ch = 'а';
+		while (ch<='я') {
+			System.out.print(ch);
+			ch++;
+		}
+		System.out.println();
+
+		System.out.println("\n");
+		//Вычисление целых степеней числа 2
+
+		int e;
+		int result;
+		for (int i9=0;i9<10;i9++) {
+			result = 1;
+			e = i9;
+			while(e>0) {
+				result *=2;
+				e--;
+			}
+			System.out.println("2 в степени " + i9 + " равно " + result);
+		}
 	}
 }
