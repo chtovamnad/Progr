46cc02339a7f22f34a2c0d18ad9f90b91dce2409
chtovamnad <vita_vita_vita@internet.ru>
2024-10-01 14:33:16 +0700

    Добавили разделы break и continue в справочную систему

--- help.java
+++ help.java
@@ -1,6 +1,7 @@
 class help {
 	public static void main(String args[]) throws java.io.IOException {
-		char choice, ignore;
+	  char choice, ignore;
+	  for(;;) {
 		do{
 			System.out.println("Справка по операторам: ");
 			System.out.println( "1. if");
@@ -8,12 +9,16 @@ class help {
 			System.out.println(" 3. for");
 			System.out.println(" 4. while");
 			System.out.println(" 5. do-while");
-			System.out.println(" Выберите номер раздела: ");
+			System.out.println(" 6. break");
+			System.out.println(" 7. continue");
+			System.out.println(" Выберите номер раздела (для завершения программы нажмите q): ");
 			choice = (char) System.in.read();
 			do{
 				ignore = (char) System.in.read();
 			} while (ignore != '\n');
-		} while (choice < '1' | choice > '5');
+		} while (choice < '1' | choice > '7' & choice != 'q');
+
+		if(choice == 'q') break;
 
 		System.out.println("\n");
 		switch(choice) {
@@ -25,16 +30,16 @@ class help {
 			case '2':
 				System.out.println("Оператор switch:\n");
 				System.out.println("switch(выражение) {");
-				System.out.println("case константа: ");
-				System.out.println(" последовательность операторов");
-				System.out.println(" break;");
+				System.out.println("	case константа: ");
+				System.out.println("	последовательность операторов");
+				System.out.println("	break;");
 				System.out.println(" ... ");
 				System.out.println("} ");
 				break;
 			case '3':
 				System.out.println("Цикл for:\n");
                                 System.out.println("for(инициализация; условие; итерация)");
-                                System.out.println("   оператор;");
+                                System.out.println("	оператор;");
 				break;
 			case '4':
 				System.out.println("Цикл while:\n");
@@ -47,9 +52,13 @@ class help {
                                 System.out.println("while (условие);");
 				System.out.println("}");
 				break;
-			default:
-				System.out.println("Неверный выбор раздела");
-		
+			case '6':
+				System.out.println("Оператор break:\n");
+				System.out.println("	break; или break метка;");
+			case '7':
+				System.out.println("Оператор continue:\n");
+				System.out.println("	continue; или continue метка;");	
 		}
+	  }
 	}
 }
