ff6bc10fdf5d341d5ba01f8f6e8d4b85d399767e
chtovamnad <vita_vita_vita@internet.ru>
2024-10-01 13:33:41 +0700

    Добавили оператор continue

--- pr003.java
+++ pr003.java
@@ -267,6 +267,21 @@ stop2:		{
 		
 		//Использование оператора continue (прерывание текущей итерации)	
 		System.out.println("\n");
+		for (int i9=0;i9<=100;i9++) {
+			if((i9%2) != 0) continue; //Переход к следующей итерации
+			System.out.println(i9 + " ");
+		}
+		System.out.println();
 
+		//continue с меткой
+		System.out.println("\n");
+outerloop:	for(int i9=1; i9<10; i9++) {
+			System.out.println("\nПроход внешнего цикла №" + i9 + ", внутренний цикл: ");
+			for(int j9=1; j9<10; j9++) {
+				if(j9==5) continue outerloop; //продолжить внешний цикл
+				System.out.println(j9);
+			}
+		}
+		System.out.println();
 	}
 }
