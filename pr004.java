d6a5bc45dde27edfdacc888baea87cfb28befc64
chtovamnad <vita_vita_vita@internet.ru>
2024-10-01 15:05:00 +0700

    Добавили новый экземпляр класса Porsche

--- pr004.java
+++ pr004.java
@@ -7,16 +7,25 @@ class pr004 {
 	public static void main(String args[]) {
 		//Создание экземпляра класса
 		Avtomobil lada = new Avtomobil();
+		Avtomobil porsche = new Avtomobil();
 		int rasst;
 
 		//Присваивание значений внутренним переменным экземпляра класса
 		lada.pass = 4;
 		lada.v = 40;
-		lada.rash = 9.5;
+		lada.rash = 5.5;
+
+		porsche.pass = 1;
+		porsche.v = 100;
+		porsche.rash = 14.0;
 
 		//Расчет расстояния, которое lada проедет на полном баке
 		rasst = (int) (lada.v/lada.rash * 100);
 
 		System.out.println("Lada проедет " + rasst + " км. на полном баке");
+		
+		rasst = (int) (porsche.v/porsche.rash * 100);
+
+                System.out.println("Porsche проедет " + rasst + " км. на полном баке");
 	}
 }
