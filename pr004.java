588e037b9fa42fe6cc8f54bc25d723d268ccbd34
chtovamnad <vita_vita_vita@internet.ru>
2024-10-01 15:28:17 +0700

    Добавили метод вычисления в класс

--- pr004.java
+++ pr004.java
@@ -1,14 +1,18 @@
 class Avtomobil {
 	int pass; //количество пассажиров
 	int v; //объем топливного бака в л.
-	double rash; // расход топлива в люъ. на 100 км.
+	double rash; // расход топлива в л. на 100 км.
+	
+	void rasst() {
+		System.out.println("Расстояние на полном баке: " + ((int)(v/rash*100)));
+	}
 }
 class pr004 {
 	public static void main(String args[]) {
 		//Создание экземпляра класса
 		Avtomobil lada = new Avtomobil();
 		Avtomobil porsche = new Avtomobil();
-		int rasst;
+		int rasst, rasst2;
 
 		//Присваивание значений внутренним переменным экземпляра класса
 		lada.pass = 4;
@@ -19,13 +23,17 @@ class pr004 {
 		porsche.v = 100;
 		porsche.rash = 14.0;
 
-		//Расчет расстояния, которое lada проедет на полном баке
-		rasst = (int) (lada.v/lada.rash * 100);
-
-		System.out.println("Lada проедет " + rasst + " км. на полном баке");
 		
-		rasst = (int) (porsche.v/porsche.rash * 100);
 
-                System.out.println("Porsche проедет " + rasst + " км. на полном баке");
+		//Расчет расстояния, которое lada проедет на полном баке
+		//rasst = (int) (lada.v/lada.rash * 100);
+
+		System.out.print("Для Lada: ");
+		lada.rasst();
+
+		//rasst2 = (int) (porsche.v/porsche.rash * 100);
+
+                System.out.print("Для Porsche: ");
+		porsche.rasst();
 	}
 }
