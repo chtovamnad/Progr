95fe2e05703c37815ce46318975ce5e9da55464b
chtovamnad <vita_vita_vita@internet.ru>
2024-10-08 14:36:58 +0700

    Программа Help в стиле ООП

--- help.java
+++ help.java
@@ -1,27 +1,6 @@
 class help {
-	public static void main(String args[]) throws java.io.IOException {
-	  char choice, ignore;
-	  for(;;) {
-		do{
-			System.out.println("Справка по операторам: ");
-			System.out.println( "1. if");
-			System.out.println(" 2. switch");
-			System.out.println(" 3. for");
-			System.out.println(" 4. while");
-			System.out.println(" 5. do-while");
-			System.out.println(" 6. break");
-			System.out.println(" 7. continue");
-			System.out.println(" Выберите номер раздела (для завершения программы нажмите q): ");
-			choice = (char) System.in.read();
-			do{
-				ignore = (char) System.in.read();
-			} while (ignore != '\n');
-		} while (choice < '1' | choice > '7' & choice != 'q');
-
-		if(choice == 'q') break;
-
-		System.out.println("\n");
-		switch(choice) {
+	void helpon(int what) {
+		switch(what) {
 			case '1':
 				System.out.println("Оператор if:\n");
 				System.out.println("if(условие) оператор;");
@@ -55,10 +34,49 @@ class help {
 			case '6':
 				System.out.println("Оператор break:\n");
 				System.out.println("	break; или break метка;");
+				break;
 			case '7':
 				System.out.println("Оператор continue:\n");
 				System.out.println("	continue; или continue метка;");	
+				break;
 		}
-	  }
+		System.out.println();
+	}
+	void showmenu() {
+			System.out.println("Справка по операторам: ");
+                        System.out.println( "1. if");
+                        System.out.println(" 2. switch");
+                        System.out.println(" 3. for");
+                        System.out.println(" 4. while");
+                        System.out.println(" 5. do-while");
+                        System.out.println(" 6. break");
+                        System.out.println(" 7. continue");
+                        System.out.println(" Выберите номер раздела (для завершения программы нажмите q): ");
+	}
+	boolean isvalid(int ch) {
+                if (ch < '1' | ch > '7' & ch != 'q') 
+			return false;
+		else
+			return true;
 	}
 }
+
+class helpclassdemo {
+	public static void main(String args[]) throws java.io.IOException {
+		char choice, ignore;
+		help hlpobj = new help();	
+		for(;;) {
+			do {
+				hlpobj.showmenu();
+				choice = (char) System.in.read();
+				do {
+					ignore = (char) System.in.read();
+				} while (ignore != '\n');
+			} while (!hlpobj.isvalid(choice));
+                if(choice == 'q') break;
+                System.out.println("\n");
+		hlpobj.helpon(choice);
+		}
+	}
+}
+
