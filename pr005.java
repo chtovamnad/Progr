bd9949f59748121812504ccba26305a1bb2d3ad5
chtovamnad <vita_vita_vita@internet.ru>
2024-10-15 14:48:06 +0700

    Добавили копирование из одного массива в другой

--- pr005.java
+++ pr005.java
@@ -112,5 +112,45 @@ class Arrays {
                                 System.out.print(riders[i3][j3] + " ");
                         System.out.println();
 		}
+
+		//Ссылки на массивы
+		System.out.println();
+		for (int i3=0;i3<10;i3++)
+			sample[i3]=i3;
+		for (int i3=0;i3<10;i3++)
+			sample1[i3]= -i3;
+
+		System.out.println("Массив sample[]: ");
+		for (int i3=0;i3<10;i3++)
+			System.out.println(sample[i3] + " ");
+		System.out.println();
+		System.out.println("Массив sample1[]: ");
+		for (int i3=0;i3<10;i3++)
+                        System.out.println(sample[i3] + " ");
+		System.out.println();
+		sample1=sample;
+		sample1[4]=1111;
+		System.out.println("Массив sample[] после изменения элемента с индексом 4 в sample1[]: ");
+		for (int i3=0; i3<10; i3++)
+			System.out.println(sample[i3] + " ");
+		System.out.println();
+		System.out.println("sample[]: "+ sample + " sample1[]: " + sample1);
+
+		//Копирование массива
+		System.out.println();
+		int[] sample3 = new int[10];
+		for (int i3=0;i3<10;i3++)
+                        sample3[i3]= -i3;
+		if(sample1.length >= sample3.length)
+			for (int i3=0; i3<sample3.length; i3++)
+				sample1[i3] = sample3[i3];
+
+		//Вывод соержимого sample1 после копирования
+		System.out.println("Массив sample[] после копирования: ");
+                for (int i3=0; i3<sample1.length; i3++)
+                        System.out.println(sample1[i3] + " ");
+                System.out.println();
+
+
 	}
 }
