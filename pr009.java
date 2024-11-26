053cc6640204e68e9779a3c4b23af5bba414d89b
chtovamnad <167408282+chtovamnad@users.noreply.github.com>
2024-11-26 11:17:11 +0700

    Закрыли переменные width и height и добавили подкласс Rectangle

--- pr009.java
+++ pr009.java
@@ -1,16 +1,32 @@
 //Основы наследования
 class TwoDShape {
-	double width;
-	double height;
+	private double width;
+	private double height;
 	void showDim() {
 		System.out.println("Ширина и высота: " + width + " и " +  height);
 	}
+	double getWidth() {
+		return width;
+	}
+	double getHeight() {
+		return height;
+	}
+	void setWidth(double w) {
+		if(w>200)
+			width = 200;
+		else width = w;
+	}
+	void setHeight(double h) {
+                if(h>290)
+                        height = 290;
+                else height = h;
+        }
 }
 class Triangle extends TwoDShape {
 	String style;
 
 	double area() {
-		return width*height/2;
+		return getWidth()*getHeight()/2;
 	}
 	void showStyle() {
 		System.out.println("Стиль: " + style);
@@ -19,10 +35,10 @@ class Triangle extends TwoDShape {
 class Rectangle extends TwoDShape {
 	String outline;
 	double area() {
-		return width*height;
+		return getWidth()*getHeight();
 	}
 	boolean isSquare() {
-		if(width == height) return true;
+		if(getWidth() == getHeight()) return true;
 		return false;
 	}
 	void showOutline() {
@@ -36,17 +52,17 @@ class pr009 {
 		Triangle t2 = new Triangle();
 		Rectangle r1 = new Rectangle();
 		Rectangle r2 = new Rectangle();
-		t1.width = 4.0;
-		t1.height = 4.0;
+		t1.setwidth = 4.0;
+		t1.setheight = 4.0;
 		t1.style = "закрашенный";
-		t2.width = 8.0;
-		t2.height = 12.0;
+		t2.setwidth = 8.0;
+		t2.setheight = 12.0;
 		t2.style = "контурный";
-		r1.width = 4.0;
-		r1.height = 4.0;
+		r1.setwidth = 4.0;
+		r1.setheight = 4.0;
 		r1.outline = "сплошная";
-		r2.width = 8.0;
-                r2.height = 12.0;
+		r2.setwidth = 8.0;
+                r2.setheight = 12.0;
                 r2.outline = "пунктирная";
 		System.out.println("Информация об объекте t1: ");
 		t1.showStyle();
